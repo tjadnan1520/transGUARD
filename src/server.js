ba9d09a6232bf0202e9624adfadf41d29bfa8af4
Tarek Jamil Adnan <bsse1520@iit.du.ac.bd>
2026-06-26 22:26:33 +0600

    Update server.jsupdated server

--- src/server.js
+++ src/server.js
@@ -3,9 +3,6 @@ import env from "./config/env.js";
 
 app.listen(env.port, () => {
     console.log(`
-======================================
-🚀 TransGuard API Started Successfully
-Environment : ${env.nodeEnv}
 Port        : ${env.port}
 URL         : http://localhost:${env.port}
 `);
